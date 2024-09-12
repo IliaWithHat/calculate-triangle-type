1b99513ff7e8dbc9c19fd08d772a0cbb36d77876
IliaWithHat <rozhkoilia5533@gmail.com>
2024-09-12 13:23:27 +0300

    Refactor test.

--- src/test/java/org/ilia/TriangleTypeCalculatorTest.java
+++ src/test/java/org/ilia/TriangleTypeCalculatorTest.java
@@ -1,8 +1,12 @@
 package org.ilia;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.math.BigInteger;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -11,11 +15,41 @@ class TriangleTypeCalculatorTest {
 
     TriangleTypeCalculator triangleTypeCalculator = new TriangleTypeCalculator();
 
-    @Test
-    void angleAreNullTest() {
-        BigInteger a = null;
-        BigInteger b = null;
-        BigInteger c = null;
+    private static Stream<Arguments> provideNullAnglesForAngleAreNullTest() {
+        return Stream.of(
+                Arguments.of(null, new BigInteger("50"), new BigInteger("50")),
+                Arguments.of(new BigInteger("50"), null, new BigInteger("50")),
+                Arguments.of(new BigInteger("50"), new BigInteger("50"), null)
+        );
+    }
+
+    private static Stream<Arguments> provideAnglesForIsoscelesTriangleTest() {
+        return Stream.of(
+                Arguments.of(new BigInteger("50"), new BigInteger("50"), new BigInteger("80")),
+                Arguments.of(new BigInteger("80"), new BigInteger("50"), new BigInteger("50")),
+                Arguments.of(new BigInteger("50"), new BigInteger("80"), new BigInteger("50"))
+        );
+    }
+
+    private static Stream<Arguments> provideRightTriangleTest() {
+        return Stream.of(
+                Arguments.of(new BigInteger("30"), new BigInteger("60"), new BigInteger("90")),
+                Arguments.of(new BigInteger("30"), new BigInteger("90"), new BigInteger("60")),
+                Arguments.of(new BigInteger("90"), new BigInteger("60"), new BigInteger("30"))
+        );
+    }
+
+    private static Stream<Arguments> provideObtuseTriangleTest() {
+        return Stream.of(
+                Arguments.of(new BigInteger("20"), new BigInteger("60"), new BigInteger("100")),
+                Arguments.of(new BigInteger("20"), new BigInteger("100"), new BigInteger("60")),
+                Arguments.of(new BigInteger("100"), new BigInteger("60"), new BigInteger("20"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideNullAnglesForAngleAreNullTest")
+    void angleAreNullTest(BigInteger a, BigInteger b, BigInteger c) {
         assertThrows(IllegalArgumentException.class, () -> triangleTypeCalculator.calculateTriangleType(a, b, c));
     }
 
@@ -35,27 +69,21 @@ class TriangleTypeCalculatorTest {
         assertEquals(TriangleType.EQUILATERAL, triangleTypeCalculator.calculateTriangleType(a, b, c));
     }
 
-    @Test
-    void isoscelesTriangleTest() {
-        BigInteger a = new BigInteger("50");
-        BigInteger b = new BigInteger("50");
-        BigInteger c = new BigInteger("80");
+    @ParameterizedTest
+    @MethodSource("provideAnglesForIsoscelesTriangleTest")
+    void isoscelesTriangleTest(BigInteger a, BigInteger b, BigInteger c) {
         assertEquals(TriangleType.ISOSCELES, triangleTypeCalculator.calculateTriangleType(a, b, c));
     }
 
-    @Test
-    void rightTriangleTest() {
-        BigInteger a = new BigInteger("30");
-        BigInteger b = new BigInteger("60");
-        BigInteger c = new BigInteger("90");
+    @ParameterizedTest
+    @MethodSource("provideRightTriangleTest")
+    void rightTriangleTest(BigInteger a, BigInteger b, BigInteger c) {
         assertEquals(TriangleType.RIGHT, triangleTypeCalculator.calculateTriangleType(a, b, c));
     }
 
-    @Test
-    void obtuseTriangleTest() {
-        BigInteger a = new BigInteger("20");
-        BigInteger b = new BigInteger("60");
-        BigInteger c = new BigInteger("100");
+    @ParameterizedTest
+    @MethodSource("provideObtuseTriangleTest")
+    void obtuseTriangleTest(BigInteger a, BigInteger b, BigInteger c) {
         assertEquals(TriangleType.OBTUSE, triangleTypeCalculator.calculateTriangleType(a, b, c));
     }
 
